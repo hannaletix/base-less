60f1485898ebbb14dbe914701df01b9dda588b0d
Hanna Letícia <hanna_45leticia@hotmail.com>
2021-04-16 02:05:19 -0300

    finalizado

---
+++ src/jsIndex.js
@@ -0,0 +1,11 @@
+function clicar(){
+    let el = document.getElementById('oculta');
+    
+    el.style.visibility = 'visible';
+}
+
+function cliqueExit(){
+    let el = document.getElementById('oculta');
+    
+    el.style.visibility = 'hidden';
+}
